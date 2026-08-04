977676e4758562e900999534eb802bcdf0368f81
Abhishek <icabhishekbrajpurohit@gmail.com>
2026-08-04 12:31:30 +0530

    updated file

--- pages/8_📊Data_Explorer.py
+++ pages/8_📊Data_Explorer.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import plotly.express as px
 from utils.data_loader import load_data
-from auth import check_password
+from utils.auth_ui import require_login
 
-if not check_password():
-    st.stop()
+require_login()
 
 st.set_page_config(page_title="Data Explorer", page_icon="📊", layout="wide")
 st.title("📊 Data Explorer")
